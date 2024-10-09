2e2c71f9a6dca4993baf53f08927e2366cf3a394
RudolphSedlin <Rudolph.Sedlin@protonmail.com>
2024-10-09 14:03:24 -0400

    Subbranching to avoid merge conflicts.

---
+++ api-router/routes/login.js
@@ -0,0 +1,46 @@
+import { Router } from "express";
+const router = Router();
+//import { userData } from "../data/index.js";
+import validation from "../validation.js";
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    //code here for GET
+    //return res.status(200).render("login", { title: "Login Page" });
+    return res.status(200);
+  })
+  .post(async (req, res) => {
+    //code here for POST
+    try {
+      let body = req.body;
+      body = validation.sanitize(body);
+
+      let email = body.emailAddressInput;
+      let password = body.passwordInput;
+
+      validation.checkNull(email);
+      validation.checkNull(password);
+
+      validation.validateEmail(email);
+      validation.validatePassword(password);
+
+      /*
+      let status = await userData.loginUser(email, password);
+
+      if (status == "Database error.")
+        return res.status(500).json({ error: "Internal server error" });
+      else {
+        res.cookie("AuthState", "Logged in!");
+        req.session.user = status;
+        return res.status(200).redirect("users");
+      }
+      */
+    } catch (error) {
+      /*return res
+        .status(400)
+        .render("login", { title: "Login Page", error: error });
+    */}
+  });
+
+export default router;
---
+++ api-router/routes/users.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+const router = Router();
+
+router.route("/").get(async (req, res) => {
+  //Todo
+  if (req.session.user){
+    /*
+    return res.status(200).render("users", {
+      title: `Home Page`,
+      firstName: req.session.user.firstName,
+      lastName: req.session.user.lastName,
+      userName: req.session.user.userName,
+      email: req.session.user.email,
+      dateOfBirth: req.session.user.dateOfBirth,
+    });
+*/
+    return res.status(200);
+    }else{
+      return res.status(400).redirect("/");
+    }
+  
+});
+
+export default router;
