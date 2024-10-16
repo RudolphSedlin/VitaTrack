31ae60218b9ec34348e20234fcd4212339aaaf9e
ZDargos <125515290+ZDargos@users.noreply.github.com>
2024-10-16 14:21:46 -0400

    Update Model_Tester.py

--- Food-NotFood_Model/Model_Tester.py
+++ Food-NotFood_Model/Model_Tester.py
@@ -4,26 +4,31 @@ from tensorflow.keras.models import load_model
 import numpy as np
 from tensorflow.keras.preprocessing import image
 
-# Load the saved model
-model = load_model('FoodData/model_1.h5')
+
 img_height, img_width = 224, 224
+# Load the saved model
+model = load_model('model_3.keras')
 
 def predict_food(img_path):
+    #Load and resize image to the proper dimensions used by the model
     img = image.load_img(img_path, target_size=(img_height, img_width))
+
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0) / 255.0  # Normalize
+
     prediction = model.predict(img_array)
-    print(prediction[0])
     if prediction[0] < 0.5:
         return "Food"
     else:
-        return "Non-food"
-path = 'FoodData/validation/Not Food/'
-Food = 0
-Not_Food = 0
-print(predict_food('FoodData/evaluation/Noodles-Pasta/building.jfif'))
-# for file in os.listdir(path):
-#     if predict_food(path + file) == "Food":
-#         Food+=1
-#     else: Not_Food+=1
-# print(f'Food: {Food}\nNot Food: {Not_Food}')
+        return "Not Food"
+
+if __name__ == "__main__":
+    path = 'FoodData/validation/Not Food/'
+    Food = 0
+    Not_Food = 0
+    print(predict_food('building.jfif'))
+    # for file in os.listdir(path):
+    #     if predict_food(path + file) == "Food":
+    #         Food+=1
+    #     else: Not_Food+=1
+    # print(f'Food: {Food}\nNot Food: {Not_Food}')
