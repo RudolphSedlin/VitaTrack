f367378d9f020a7a80c4853241dda8732c15d46a
RudolphSedlin <Rudolph.Sedlin@protonmail.com>
2024-10-21 18:30:23 -0400

    Much needed cleaning of the API.

--- API/routes/users.js
+++ API/routes/users.js
@@ -1,32 +1,13 @@
 import { Router } from "express";
 const router = Router();
 
-router.route("/").get(async (req, res) => {
-  if (req.session.user){
+router
+.route("/")
+.get(async (req, res) => {
+  if (req.session.user)
+    return res.status(200).json(req.session.user);
 
-    return res.status(200).json({
-      firstname: req.session.user.firstname,
-      lastname: req.session.user.lastname,
-      phonenumber: req.session.user.phonenumber,
-      state: req.session.user.state,
-      meals: req.session.user.meals,
-
-      address: req.session.user.address,
-      gender: req.session.user.gender,
-      dateOfBirth: req.session.user.dateOfBirth,
-      doctorname: req.session.user.doctorname,
-      conditions: req.session.user.conditions,
-      consentLetter: req.session.user.consentLetter,
-
-      email: req.session.user.email,
-      height: req.session.user.height,
-      weight: req.session.user.weight,
-    })
-    }
-
-    else{
-      return res.status(400).json({error: "Not authenticated."});
-    }
+  return res.status(403).send("Not authenticated!");
   
 });
 
