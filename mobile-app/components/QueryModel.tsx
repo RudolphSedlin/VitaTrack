e2841171c261a199a43a1c64d91fc7381b0327a3
kaikune <alexkai03@live.com>
2024-10-27 23:47:52 -0400

    Integrated image model to work on frontent

--- mobile-app/components/QueryModel.tsx
+++ mobile-app/components/QueryModel.tsx
@@ -5,78 +5,75 @@ import TensorCamera from "./TensorCamera"; // Assuming TensorCamera is in the sa
 import { useToast } from "react-native-toast-notifications";
 
 function QueryModel() {
-    const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState("No prediction yet");
-    const [prediction, setPrediction] = useState(null);
-    const [pictureTaken, setPictureTaken] = useState(false);
-    const toast = useToast();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("No prediction yet");
+  const [prediction, setPrediction] = useState(null);
+  const [pictureTaken, setPictureTaken] = useState(false);
+  const toast = useToast();
 
-    // Function to handle new predictions
-    const handlePrediction = (predictions: number[][]) => {
-        console.log(predictions);
-        if (predictions && predictions.length > 0) {
-            const prediction = predictions[0][0];
+  // Function to handle new predictions
+  const handlePrediction = (predictions: number[][]) => {
+    console.log(predictions);
+    if (predictions && predictions.length > 0) {
+      const prediction = predictions[0][0];
 
-            // Check if the highest probability is above 50%
-            if (prediction < 0.5) {
-                setMessage(
-                    `${prediction}`,
-                );
-                
+      // Check if the highest probability is above 50%
+      if (prediction < 0.5) {
+        setMessage(`${prediction}`);
 
-                if (!pictureTaken) {
-                    //TODO: take picture
+        if (!pictureTaken) {
+          //TODO: take picture
 
-                    toast.show("Picture taken");
-                    setPictureTaken(true);
-                }
-            } else {
-                setMessage("No food");
-                toast.show("No food");
-            }
+          toast.show("Picture taken");
+          setPictureTaken(true);
         }
-    };
+      } else {
+        setMessage("No food");
+        toast.show("No food");
+      }
+    }
+  };
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tensor Model Prediction</Text>
-            {/* TensorCamera component with setPrediction passed as a prop */}
-            <TensorCamera setPrediction={handlePrediction} />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tensor Model Prediction</Text>
+      {/* TensorCamera component with setPrediction passed as a prop */}
+      <TensorCamera setPrediction={handlePrediction} />
+    </View>
+  );
 }
 
 export default QueryModel;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#f0f0f0",
-        padding: 16,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "bold",
-        marginBottom: 16,
-    },
-    message: {
-        fontSize: 18,
-        marginVertical: 10,
-        paddingHorizontal: 10,
-        textAlign: "center",
-    },
-    resetButton: {
-        marginTop: 20,
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        backgroundColor: "#007bff",
-        borderRadius: 8,
-    },
-    buttonText: {
-        color: "#fff",
-        fontSize: 16,
-        fontWeight: "bold",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  message: {
+    fontSize: 18,
+    marginVertical: 10,
+    paddingHorizontal: 10,
+    textAlign: "center",
+  },
+  resetButton: {
+    marginTop: 20,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    backgroundColor: "#007bff",
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
 });
--- mobile-app/components/TensorCamera.tsx
+++ mobile-app/components/TensorCamera.tsx
@@ -3,246 +3,313 @@ import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
 import { useNavigation } from "expo-router";
 import { useEffect, useRef, useState } from "react";
 import {
-    Button,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    Dimensions,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions,
 } from "react-native";
-import * as mobilenet from "@tensorflow-models/mobilenet";
 import * as tf from "@tensorflow/tfjs";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCircleUser, faLightbulb as faRegularBulb } from "@fortawesome/free-regular-svg-icons"
-import { faCamera, faXmark, faLightbulb } from "@fortawesome/free-solid-svg-icons"
+import {
+  faCircleUser,
+  faLightbulb as faRegularBulb,
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faCamera,
+  faXmark,
+  faLightbulb,
+} from "@fortawesome/free-solid-svg-icons";
 import { Tensor3D } from "@tensorflow/tfjs";
+import {
+  bundleResourceIO,
+  base64ImageToTensor,
+} from "@tensorflow/tfjs-react-native";
 import { useToast } from "react-native-toast-notifications";
-import { loadGraphModel } from '@tensorflow/tfjs-converter';
+import { loadGraphModel } from "@tensorflow/tfjs-converter";
+import * as jpeg from "jpeg-js";
+import * as ImageManipulator from "expo-image-manipulator";
 
+const modelJSON = require("../assets/food_model.json/model.json");
+const modelWeights = require("../assets/food_model.json/group1.bin");
 
-library.add(faCircleUser)
-library.add(faCamera)
-library.add(faXmark)
-library.add(faLightbulb)
-library.add(faRegularBulb)
+library.add(faCircleUser);
+library.add(faCamera);
+library.add(faXmark);
+library.add(faLightbulb);
+library.add(faRegularBulb);
 
 type TensorCameraProps = {
-    setPrediction: (preds: number[][]) => void;
-}
+  setPrediction: (preds: number[][]) => void;
+};
 
 export default function TensorCamera(props: TensorCameraProps) {
-    const [lightState, setLightState] = useState(false);
-    const [facing, setFacing] = useState<CameraType>("back");
-    const [permission, requestPermission] = useCameraPermissions();
-    const [model, setModel] = useState<mobilenet.MobileNet | null>(null);
-    const [loopRunning, setLoopRunning] = useState(false);
-    const cameraRef = useRef<CameraView>(null); // Camera reference
-    const isRunning = useRef(false); // Controls loop status
-    const toast = useToast();
+  const [lightState, setLightState] = useState(false);
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const [model, setModel] = useState<mobilenet.MobileNet | null>(null);
+  const [loopRunning, setLoopRunning] = useState(false);
+  const cameraRef = useRef<CameraView>(null); // Camera reference
+  const isRunning = useRef(false); // Controls loop status
+  const toast = useToast();
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const setPrediction = props.setPrediction;
+  const setPrediction = props.setPrediction;
 
-    // Load TensorFlow.js model
-    useEffect(() => {
-        (async () => {
-            console.log("Test")
-            await tf.ready(); // Ensure TensorFlow.js is ready
-            const loadedModel = await loadGraphModel('food_model');
-            setModel(loadedModel);
-            console.log("Successfully loaded model");
-        })();
-    }, []);
-
-    const imageToTensor = (imageData: string): Promise<tf.Tensor3D> => {
-        return new Promise((resolve, reject) => {
-            // Create a new Image object
-            const img = new Image();
-
-            // Set the source of the image
-            img.src = imageData;
-
-            // Handle image loading
-            img.onload = () => {
-                // Convert the loaded image to a Tensor
-                const imageFeatures = tf.tidy(() => {
-                    const imageAsTensor = tf.browser.fromPixels(img);
-                    return imageAsTensor;
-                });
-                resolve(imageFeatures); // Resolve the promise with the tensor
-            };
-
-            // Handle image loading errors
-            img.onerror = (error) => {
-                console.error("Error loading image:", error);
-                reject(error); // Reject the promise with the error
-            };
-        });
-    };
-
-    const runModelPrediction = async (imageTensor: Tensor3D) => {
-        if (model) {
-            const prediction = model.predict(imageTensor);
-            return prediction;
-        }
-    };
-
-    const captureAndAnalyzeFrame = async () => {
-        if (cameraRef.current) {
-            // Capture the frame
-            const picture = await cameraRef.current.takePictureAsync({
-                base64: true,
-                quality: 0.5, // Adjust to balance quality and performance
-            });
-
-            if (picture && picture.base64) {
-                const tensor = await imageToTensor(picture.base64);
-                const prediction = await runModelPrediction(tensor);
-
-                // Use the prediction (e.g., update UI)
-                setPrediction(prediction);
-
-                // Dispose of tensor to free memory
-                tensor.dispose();
-            }
-        }
-    };
-
-    const startFrameLoop = async () => {
-        isRunning.current = true;
-
-        const processFrame = async () => {
-            if (!isRunning.current || !cameraRef.current) return;
-
-            await captureAndAnalyzeFrame(); // Capture and analyze current frame
-
-            // Run the next frame in the loop
-            requestAnimationFrame(processFrame); // Schedule the next frame
-        };
-
-        // Start the loop
-        requestAnimationFrame(processFrame);
-    };
-
-    const stopFrameLoop = () => {
-        isRunning.current = false;
-    };
-
-    function toggleFrameLoop() {
-        if (loopRunning) {
-            toast.show("Frame loop started!");
-            stopFrameLoop();
-        } else {
-            toast.show("Frame loop stopped!");
-            startFrameLoop();
-        }
-        setLoopRunning(!loopRunning)
-    }
-
-    // Camera permission handling
-    if (!permission) {
-        return <View />;
-    }
-
-    if (!permission.granted) {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.message}>
-                    We need your permission to show the camera
-                </Text>
-                <Button onPress={requestPermission} title="grant permission" />
-            </View>
+  // Load TensorFlow.js model
+  useEffect(() => {
+    (async () => {
+      await tf.ready(); // Ensure TensorFlow.js is ready
+      try {
+        const loadedModel = await tf.loadLayersModel(
+          bundleResourceIO(modelJSON, modelWeights),
         );
-    }
+        setModel(loadedModel);
+        console.log("Successfully loaded model");
+      } catch (e) {
+        console.error("[LOADING ERROR] info:", e);
+      }
+    })();
+  }, []);
 
-    function toggleCameraFacing() {
-        setFacing((current) => (current === "back" ? "front" : "back"));
-    }
-
-    function toggleFlashlight() {
-        setLightState(!lightState);
-    }
-
-    return (
-        <View>
-            <CameraView enableTorch={lightState} testID="camera-view" style={styles.camera} facing={facing} ref={cameraRef}>
-                <View style={{
-                    position: 'absolute',
-                    bottom: 0,
-                    flexDirection: 'row',
-                    flex: 1,
-                    width: '100%',
-                    padding: 20,
-                    justifyContent: 'space-between'
-                }}>
-                    <View style={{
-                        alignSelf: 'center',
-                        flex: 1,
-                        alignItems: 'center',
-                        flexDirection: "row",
-                        justifyContent: "space-between"
-                    }}>
-                        <TouchableOpacity style={{
-                            width: 60,
-                            height: 60,
-                            bottom: 50,
-                            borderRadius: 100,
-                            backgroundColor: '#fff',
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }} onPress={toggleFlashlight}>
-                            <FontAwesomeIcon size={30} icon={lightState ? ["fas", "lightbulb"] : ["far", "lightbulb"]} />
-                        </TouchableOpacity>
-
-                        <TouchableOpacity style={{
-                            width: 90,
-                            height: 90,
-                            bottom: 50,
-                            borderRadius: 100,
-                            backgroundColor: '#fff',
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }} onPress={toggleFrameLoop}>
-                            <FontAwesomeIcon size={40} icon={["fas", "camera"]} />
-                        </TouchableOpacity>
-
-                        <TouchableOpacity style={{
-                            width: 60,
-                            height: 60,
-                            bottom: 50,
-                            borderRadius: 100,
-                            backgroundColor: '#fff',
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }} onPress={navigation.goBack}>
-                            <FontAwesomeIcon color="red" size={30} icon={["fas", "xmark"]} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-
-            </CameraView>
-        </View>
+  const resizeImage = async (
+    imageUrl: string,
+    width: number,
+    height: number,
+  ): Promise<ImageManipulator.ImageResult> => {
+    const actions = [
+      {
+        resize: {
+          width,
+          height,
+        },
+      },
+    ];
+    const saveOptions = {
+      compress: 0.75,
+      format: ImageManipulator.SaveFormat.JPEG,
+      base64: true,
+    };
+    const res = await ImageManipulator.manipulateAsync(
+      imageUrl,
+      actions,
+      saveOptions,
     );
+    return res;
+  };
+
+  const base64ImageToTensor = async (base64: string): Promise<tf.Tensor4D> => {
+    // Decode the base64 image
+    const rawImageData = tf.util.encodeString(base64, "base64");
+    const TO_UINT8ARRAY = true;
+    const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY);
+
+    // Drop the alpha channel info if present
+    const buffer = new Uint8Array(width * height * 3);
+    let offset = 0; // offset into original data
+    for (let i = 0; i < buffer.length; i += 3) {
+      buffer[i] = data[offset];
+      buffer[i + 1] = data[offset + 1];
+      buffer[i + 2] = data[offset + 2];
+      offset += 4; // Skip the alpha channel
+    }
+
+    // Create a 3D tensor from the buffer
+    const imageTensor = tf.tensor3d(buffer, [height, width, 3]);
+
+    // Add a batch dimension to create a 4D tensor
+    const batchedImageTensor = imageTensor.expandDims(0); // This creates a tensor of shape [1, height, width, 3]
+
+    return batchedImageTensor;
+  };
+
+  const runModelPrediction = async (imageTensor: Tensor3D) => {
+    if (model) {
+      const prediction = await model.predict(imageTensor).data();
+      return prediction;
+    }
+  };
+
+  const captureAndAnalyzeFrame = async () => {
+    if (cameraRef.current) {
+      // Capture the frame
+      const picture = await cameraRef.current.takePictureAsync({
+        base64: true,
+        quality: 1, // Adjust to balance quality and performance
+      });
+
+      if (picture && picture.base64) {
+        // const tensor = await imageToTensor(picture.base64);
+        const resized = await resizeImage(picture.uri, 224, 224);
+        const tensor = await base64ImageToTensor(resized.base64);
+        const prediction = await runModelPrediction(tensor);
+
+        // Use the prediction (e.g., update UI)
+        setPrediction(prediction);
+
+        // Dispose of tensor to free memory
+        tensor.dispose();
+      }
+    }
+  };
+
+  const startFrameLoop = async () => {
+    isRunning.current = true;
+
+    const processFrame = async () => {
+      if (!isRunning.current || !cameraRef.current) return;
+
+      await captureAndAnalyzeFrame(); // Capture and analyze current frame
+
+      // Run the next frame in the loop
+      requestAnimationFrame(processFrame); // Schedule the next frame
+    };
+
+    // Start the loop
+    requestAnimationFrame(processFrame);
+  };
+
+  const stopFrameLoop = () => {
+    isRunning.current = false;
+  };
+
+  function toggleFrameLoop() {
+    if (loopRunning) {
+      toast.show("Frame loop started!");
+      stopFrameLoop();
+    } else {
+      toast.show("Frame loop stopped!");
+      startFrameLoop();
+    }
+    setLoopRunning(!loopRunning);
+  }
+
+  // Camera permission handling
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  function toggleFlashlight() {
+    setLightState(!lightState);
+  }
+
+  return (
+    <View>
+      <CameraView
+        enableTorch={lightState}
+        testID="camera-view"
+        style={styles.camera}
+        facing={facing}
+        ref={cameraRef}
+      >
+        <View
+          style={{
+            position: "absolute",
+            bottom: 0,
+            flexDirection: "row",
+            flex: 1,
+            width: "100%",
+            padding: 20,
+            justifyContent: "space-between",
+          }}
+        >
+          <View
+            style={{
+              alignSelf: "center",
+              flex: 1,
+              alignItems: "center",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                width: 60,
+                height: 60,
+                bottom: 50,
+                borderRadius: 100,
+                backgroundColor: "#fff",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onPress={toggleFlashlight}
+            >
+              <FontAwesomeIcon
+                size={30}
+                icon={lightState ? ["fas", "lightbulb"] : ["far", "lightbulb"]}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={{
+                width: 90,
+                height: 90,
+                bottom: 50,
+                borderRadius: 100,
+                backgroundColor: "#fff",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onPress={toggleFrameLoop}
+            >
+              <FontAwesomeIcon size={40} icon={["fas", "camera"]} />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={{
+                width: 60,
+                height: 60,
+                bottom: 50,
+                borderRadius: 100,
+                backgroundColor: "#fff",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onPress={navigation.goBack}
+            >
+              <FontAwesomeIcon color="red" size={30} icon={["fas", "xmark"]} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </CameraView>
+    </View>
+  );
 }
 
 const { width, height } = Dimensions.get("window");
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    message: {
-        textAlign: "center",
-        paddingBottom: 10,
-    },
-    camera: {
-        width: width,
-        height: height,
-        borderRadius: 10,
-        zIndex: 0,
-        elevation: 0,
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    width: width,
+    height: height,
+    borderRadius: 10,
+    zIndex: 0,
+    elevation: 0,
+  },
 });
