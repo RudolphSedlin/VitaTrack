ea47e8f126977eeffd7cb4e73cb7dbe1cadec350
RudolphSedlin <Rudolph.Sedlin@protonmail.com>
2024-11-14 14:51:14 -0500

    raise limit for express requests.

--- API/app.js
+++ API/app.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import crypto from "crypto";
 
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb'})); // Necessary for larger payloads with image data.
 
 app.use(
