c14d2fc6290948a3e1533952e334ac99c76a6f9b
RudolphSedlin <Rudolph.Sedlin@protonmail.com>
2024-11-22 18:17:07 -0500

    fix issues arising from obfuscated secret.

--- API/routes/meals.js
+++ API/routes/meals.js
@@ -7,6 +7,7 @@ import { BREAD } from "../resources/IMAGES.js";
 import validation from "../validation.js";
 import 'dotenv/config'
 
+console.log(process.env);
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 if (!OPENAI_API_KEY)
